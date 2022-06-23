97327725e8e72c68fa639c1bf3597b3f152a38fe
thimakulani <thimakulani@gmail.com>
2022-06-23 14:44:00 +0200

    user auth

--- farm_web_api/models/UserLogin.cs
+++ farm_web_api/models/UserLogin.cs
@@ -2,8 +2,11 @@
 {
     public class UserLogin
     {
-        public int Id { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; } 
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
